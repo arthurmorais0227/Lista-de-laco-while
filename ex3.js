10bb949b883647ec64c7d964ce4d7295e3840705
Arthur Morais <mlkliso4000@gmail.com>
2025-04-11 10:41:23 -0300

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -4,8 +4,8 @@ let numerossomados = 0
 
 while (soma <= 30) {
     soma += numerossomados;
-    numerossomados++
-    quantidadeTentativas++
-    console.log(`${soma} + ${numerossomados} = ${soma + numerossomados}. Foram utilizados ${quantidadeTentativas} números.`)
+    numerossomados++;
+    quantidadeTentativas++;
+    console.log(`${soma} + ${numerossomados} = ${soma + numerossomados}. Foram utilizados ${quantidadeTentativas} números.`);
 }
 console.log(`Soma final: ${soma + numerossomados}.`)
